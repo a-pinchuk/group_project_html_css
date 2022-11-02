ff43eaba0e72b8614cd194e052f68f5eaf7640b4
a-pinchuk <co.born2code@gmail.com>
2022-11-02 16:32:29 +0000

    Deploying to gh-pages from @ a-pinchuk/group_project_html_css@f93a033a2b683f1eb6b59181a3fb2a36f2e33760 🚀

---
+++ index.2afe983f.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalHeroBtn:document.querySelector("[data-modal-open-hero]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.openModalHeroBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")})),e.closeModalBtn.addEventListener("click",toggleModal)}();
+//# sourceMappingURL=index.2afe983f.js.map
