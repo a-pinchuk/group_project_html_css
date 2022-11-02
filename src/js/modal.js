79e374cb7b79e41033e1e64db8471ea9437b0bd0
a-pinchuk <co.born2code@gmail.com>
2022-11-02 19:44:58 +0200

    edit js file for modal window

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,46 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
+// (() => {
+//   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+//     e.preventDefault();
+
+//     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+
+//     e.currentTarget.reset();
+//   });
+// })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  for (var i = 0; i < refs.openModalBtn.length; i++) {
+    refs.openModalBtn[i].addEventListener('click', toggleModal);
+  
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+}
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
-})();
+
+    })
+
+  ();
 
 (() => {
   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
